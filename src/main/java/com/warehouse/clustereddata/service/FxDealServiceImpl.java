3496c9ec1898f6abc244bb0f91e0ce3cd9816de1
Suleiman Obeidat <sleman.obaidat@gmail.com>
2024-05-13 20:53:04 +0300

    Adding logs

--- src/main/java/com/warehouse/clustereddata/service/FxDealServiceImpl.java
+++ src/main/java/com/warehouse/clustereddata/service/FxDealServiceImpl.java
@@ -3,6 +3,8 @@ package com.warehouse.clustereddata.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,29 +14,38 @@ import com.warehouse.clustereddata.repo.FxDealRepository;
 @Service("dealService")
 public class FxDealServiceImpl implements FxDealService {
 
+	private static final Logger logger = LoggerFactory.getLogger(FxDealServiceImpl.class);
+
 	@Autowired
 	private FxDealRepository dealRepository;
 	   
 	@Override
 	public FxDeal processDeal(FxDeal deal) {
 		if(isDealExists(deal.getDealId())) {
+			logger.warn("Deal with ID {} already exists. Skipping processing.", deal.getDealId());
 			return null;
 		}
-		
+
+		logger.info("Processing deal with ID {}", deal.getDealId());
 		return saveDeal(deal);
 	}
 
 	@Override
 	public FxDeal saveDeal(FxDeal deal) {
+		logger.info("Saving deal with ID {}", deal.getDealId());
 		return dealRepository.save(deal);
 	}
 
 	@Override
 	public FxDeal getDealById(Long id) {
+		logger.info("Retrieving deal with ID {}", id);
+
 		Optional<FxDeal> deal = dealRepository.findById(id);
 		if(deal.isPresent()) {
 			return deal.get();
-		}		
+		}
+
+		logger.warn("Deal with ID {} not found", id);
 		return null;
 	}
 
@@ -46,6 +57,5 @@ public class FxDealServiceImpl implements FxDealService {
 	@Override
 	public boolean isDealExists(String dealId) {
 		return dealRepository.findByDealId(dealId) != null;
-	}
-	
+	}	
 }
